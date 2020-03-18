12831a5150e50ba7c87dd78f86dcc20dce9e158e
Takuto Okuno <takuto.okuno.ku0823@gmail.com>
2020-03-18 15:31:46 +0900

    feat: add .tmux.conf

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ mkdir -p ~/.zsh
 ln -fsv ~/.dotfiles/src/.zsh/.zshrc	~/.zsh/.zshrc
 ln -fsv ~/.dotfiles/src/.zshenv		~/.zshenv
 ln -fsv ~/.dotfiles/src/.gitconfig	~/.gitconfig
+ln -fsv ~/.dotfiles/src/.tmux.conf      ~/.tmux.conf
